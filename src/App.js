2c89a4975b9964416c615fdb8b941ec1117662c2
vickyrockstar <116479689+vickyrockstar@users.noreply.github.com>
2023-03-23 13:06:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,35 +1,72 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Element from './components/element';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [currpop, setPop] = useState("");
+  const [currtop, setTop] = useState("");
+  const [stack, setStack] = useState("");
+
+  const handleChange =(e) => {
+    var curr = e.target.value;
+    setValue(curr);
+  };
+  const getTop = () => {
+    if(stack.length === 0) {
+      alert("Stack is empty");
+      return;
+    }
+    var top = stack[0];
+    setTop(top);
+  };
+  const push = () => {
+    if(value === ""){
+      alert("Please enter a value");
+      return;
+    }
+    setStack([value, ...stack]);
+  };
+  const pop = () => {
+    if(stack.length === 0){
+      alert("Stack is empty");
+      return;
+    }
+    var top = stack[0];
+    var arr = stack.slice();
+    arr.shift();
+    setStack(arr);
+    setPop(top);
+  };
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
       <div className="row">
         <div className="col md-6 stack">
-          <Element />
+          { stack.map((item, key) =>(
+          <Element key={key} val={item}/>
+          ))}
         </div>
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={push}>
               PUSH
             </button>
-            <input type="text" className="form-control" />
+            <input type="text" value={value} onChange={handleChange} className="form-control" />
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button onClick={pop} className="btn btn-outline-secondary" type="button" id="button-addon2">
               POP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input value={currpop} type="text" disabled className="form-control" />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button onClick={getTop} className="btn btn-outline-secondary" type="button" id="button-addon2">
               TOP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input value={currtop} type="text" disabled className="form-control" />
           </div>
         </div>
       </div>
